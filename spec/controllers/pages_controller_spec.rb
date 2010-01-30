b3ff349c8dbb2e30ed4f5f56a763ee8141af8e39
Pelle Bergkvist <pelleberka@gmail.com>
2010-01-30 13:52:43 +0100

    Removed default Rails page

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,6 +3,10 @@ require 'spec_helper'
 describe PagesController do
   integrate_views
 
+  before(:each) do
+    @base_title = "Ruby on Rails Tutorial Sample App"
+  end
+
   #Delete these examples and add some real ones
   it "should use PagesController" do
     controller.should be_an_instance_of(PagesController)
@@ -17,7 +21,7 @@ describe PagesController do
 
     it "should have the right title" do
       get 'home'
-      response.should have_tag("title", "Ruby on Rails Tutorial Sample App | Home")
+      response.should have_tag("title", @base_title + " | Home")
     end
   end
 
